e946a05bd8e317dfba76f5e2c14ae1c545fcb64d
TheSmoothWater <thesmoothwater@gmail.com>
2025-04-17 17:12:32 -0400

    Issue complete

--- src/Main.py
+++ src/Main.py
@@ -289,6 +289,10 @@ def waitForSecondConnection():
 
 
 def getServerAddress() -> tuple[str, int]:
+    """
+    Gets the server address from user input
+    :return: (serverIp,serverPort) a tuple of the server's address
+    """
     serverIp: str
     while True:
         try:
